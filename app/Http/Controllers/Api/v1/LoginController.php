a84cac14832105a3ed5b428dc241daa715eb2d09
Reksi Arismunandar <reksihu@gmail.com>
2024-06-05 22:42:27 +0700

    login controller

---
+++ app/Http/Controllers/Api/v1/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index(Request $request) {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            // return $user->id;
+            // dd($user);
+            $issuedTokens = PersonalAccessToken::where('tokenable_type', 'App\Models\User')
+            ->where('tokenable_id', $user->id)
+            ->whereBetween('created_at', [
+                Carbon::today(),
+                Carbon::today()->endOfDay()
+            ])
+            ->get();
+            
+            if ($issuedTokens->count() > 10) {
+                return response()->json(['message' => 'Max Limit Today.']);
+            }
+
+            $basicToken = $user -> createToken('basic-token', ['none'], expiresAt: now()->addMinutes(30));
+            return [
+                'basic' => $basicToken->plainTextToken
+            ];
+        } else {
+            return response()->json(['message' => 'Unauthenticated.']);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,37 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Sanctum\PersonalAccessToken;
-use Carbon\Carbon;
+use App\Http\Controllers\Api\v1\LoginController;
 
-Route::post('/generate-token', function (Request $request) {
-    $credentials = [
-        'email' => $request->email,
-        'password' => $request->password
-    ];
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
-        // return $user->id;
-        // dd($user);
-        $issuedTokens = PersonalAccessToken::where('tokenable_type', 'App\Models\User')
-        ->where('tokenable_id', $user->id)
-        ->whereBetween('created_at', [
-            Carbon::today(),
-            Carbon::today()->endOfDay()
-        ])
-        ->get();
-        
-        if ($issuedTokens->count() > 10) {
-            return response()->json(['message' => 'Max Limit Today.']);
-        }
-
-        $basicToken = $user -> createToken('basic-token', ['none'], expiresAt: now()->addMinutes(30));
-        return [
-            'basic' => $basicToken->plainTextToken
-        ];
-    } else {
-        return response()->json(['message' => 'Unauthenticated.']);
-    }
+Route::group(['prefix' => 'v1'], function() {
+    Route::post('generate-token', [LoginController::class, 'index']);
 });
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\v1', 'middleware' => 'auth:sanctum'], function() {
